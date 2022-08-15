b576df7c2df021ccd67ce217d5bcb775c3c2aeee
terryrao <7406974+terryrao@users.noreply.github.com>
2022-08-16 00:37:52 +0800

    go config

---
+++ lisp/init-lsp.el
@@ -0,0 +1,52 @@
+;; program-lsp config
+
+;;; Code:
+
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (go-mode . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+(use-package go-mode)
+
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (display-line-numbers-mode)
+            (electric-layout-mode)
+            (electric-pair-mode)
+            (hs-minor-mode)
+            (prettify-symbols-mode)
+(set (make-local-variable 'company-backends) '(company-go))
+        (company-mode)))
+
+
+(provide 'init-lsp)
+
