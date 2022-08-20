fdbbba24f4ed8463f60db5421434602dee4e188f
terryrao <7406974+terryrao@users.noreply.github.com>
2022-08-20 19:41:09 +0800

    remove custome.el from git

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(projectile which-key use-package smart-mode-line restart-emacs popwin lsp-ui lsp-ivy ivy-posframe gruvbox-theme go-mode flycheck dumb-jump dap-mode crux counsel company command-log-mode benchmark-init)))
+   '(smart-mode-line gruvbox-theme evil dired-sidebar projectile popwin ace-window ivy-posframe which-key crux flycheck dumb-jump company counsel ivy restart-emacs use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
